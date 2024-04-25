51c6122d5c1d4ff694d195025744d01b85869c74
shree-varshaa-k <156644283+shree-varshaa-k@users.noreply.github.com>
2024-04-24 21:11:29 -0700

    updated

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import express from "express";
 import connectDb from "./config/db.js";
-import Todo from "./model/TodoModel.js";
+import todoRoutes from "./routes/todoRoutes.js"
 import Varsha from "./model/varsha.js";
 
 connectDb();
 const  app =express();
 app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('app is running')
 })
@@ -13,15 +14,15 @@ app.get('/varsha', (req, res) => {
   console.log(req.body)
   res.send({name:"varsha",age:"24"})
 })
-app.post("/api/todo",async (req,res)=>{
-  console.log(req.body);
-const todo =await Todo.create({
+// app.post("/api/todo",async (req,res)=>{
+//   console.log(req.body);
+// const todo =await Todo.create({
   
-title:req.body.title,
-description:req.body.desc,
-})
-res.json(todo);
-})
+// title:req.body.title,
+// description:req.body.desc,
+// })
+// res.json(todo);
+// })
 
 app.post("/api/varsha",async (req,res)=>{
   console.log(req.body);
@@ -32,12 +33,11 @@ app.post("/api/varsha",async (req,res)=>{
   })
   res.json(varsha);
   })
-  app.get('/api/todo',async (req, res) => {
-   const todos=await Todo.find();
-   res.json(todos);
-  })
-
-
+  // app.get('/api/todo',async (req, res) => {
+  //  const todos=await Todo.find();
+  //  res.json(todos);
+  // })
+app.use("/api/todo",todoRoutes);
 app.listen(8000, () => {
   console.log("server running in 8000" );
 })
