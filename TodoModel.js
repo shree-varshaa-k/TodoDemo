1ed2f7f6ab0ebd9e91ff77a8eedc95b34c84026c
shree-varshaa-k <156644283+shree-varshaa-k@users.noreply.github.com>
2024-04-24 21:12:24 -0700

    Add files via upload

---
+++ TodoModel.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+
+const todoSchema =mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+    },
+    description:{
+        type:String,
+        required:true,
+    },  
+    status:{
+        type:Boolean,
+        required:true,
+        default:false,
+    },  
+
+})
+const Todo =mongoose.model("Todo",todoSchema);
+export default Todo;
