a1a167d1c3d09df48dfcffece7a95cb91ec08929
shree-varshaa-k <156644283+shree-varshaa-k@users.noreply.github.com>
2024-04-24 21:11:54 -0700

    Add files via upload

---
+++ todoRoutes.js
@@ -0,0 +1,30 @@
+import express from "express";
+import {
+  addTodo,
+  getTodo,
+  deleteTodo,
+} from "../controllers/todoControllers.js";
+
+const router = express.Router();
+
+router.route("/").get(getTodo).post(addTodo);
+router.route("/:id").delete(deleteTodo);
+
+// router.get("/",getTodo);
+// router.post("/",addTodo);
+
+// router.get("/",
+// async (req, res) => {
+//     const todos=await Todo.find();
+//     res.json(todos);
+//    })
+//    router.post("/",async (req,res)=>{
+//     console.log(req.body);
+//   const todo =await Todo.create({
+
+//   title:req.body.title,
+//   description:req.body.desc,
+//   })
+//   res.json(todo);
+//   })
+export default router;
