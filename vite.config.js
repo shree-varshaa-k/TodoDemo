aa857dbd4db90a514439d7438b9effc44625634c
shree-varshaa-k <156644283+shree-varshaa-k@users.noreply.github.com>
2024-05-07 22:23:13 -0700

    frontend

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-server: {
-  proxy: {
-    '/api': {
-      target: '',
-      changeOrigin: true,
-      
-    },
-  }
-}
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000/",
+        changeOrigin: true,
+      },
+    },
+  },
 });
